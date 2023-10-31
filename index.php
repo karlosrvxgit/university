f4173e954877b6aeab6ab86d0e9bec829b8e5af4
Carlos Rojas Vega <karlosrvxx@gmail.com>
2023-10-31 01:03:35 -0500

    updates final

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>UNIVERSITY</title>
     <link href="/dist/output.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,1,0" />
 </head>
 
 <body class="min-h-screen flex flex-col items-center justify-center bg-yellow-100">
@@ -13,29 +14,23 @@
         <img src="/imagenes/logo.jpg" alt="Logo" class="w-full">
     </div>
     <form action="/login/login.php" method="post" class="bg-white rounded-lg shadow-lg max-w-xs w-full p-8">
-        <div class="mb-4 ">
-            <div class="mb='4' flex justify-center text-sm text-gray-600">
-            <p>Bienvenido ingresa con tu cuenta</p>
-            <div>
-            <span></span>
-            </div>
-            <span></span>
-            </div>
-            
-            <label for="correo" class="block text-sm text-gray-600">Correo</label>
-            <input type="email" name="correo" id="correo"
-                class="w-full py-2 px-4 rounded-full border-2 text-sm focus:outline-none focus:border-violet-700"
-                required>
-        </div>
         <div class="mb-4">
-            <label for="contrasena" class="block text-sm text-gray-600">Contraseña</label>
-            <input type="password" name="contrasena" id="contrasena"
-                class="w-full py-2 px-4 rounded-full border-2 text-sm focus:outline-none focus:border-violet-700"
-                required>
+            <p class="mb-4 text-center text-gray-600">Bienvenido, ingresa con tu cuenta</p>
+            <div class="flex  mb-4  ">
+                <input placeholder="Email" name="correo" id="correo" class="w-full py-2 px-4 rounded-full border-2 text-sm focus:outline-none focus:border-violet-700" required>
+                <span class="material-symbols-outlined right-12 z-01 relative top-3 text-gray-300">
+                    mail
+                </span>
+            </div>
+            <div class="relative flex">
+                <input type="password" placeholder="Password" name="contrasena" id="contrasena" class="w-full py-2 px-4 rounded-full border-2 text-sm focus:outline-none focus:border-violet-700" required>
+                <span class="material-symbols-outlined right-12 z-01 relative top-3 text-gray-300">
+                    lock
+                </span>
+            </div>
         </div>
         <div class="mb-4 flex items-center justify-between">
-            <button type="submit"
-                class="w-32 py-2 bg-yellow-500 hover:bg-blue-600 active:bg-blue-700 focus:outline-none focus:ring focus:ring-blue-500 rounded-full text-white">
+            <button type="submit" class="w-32 py-2 bg-yellow-500 hover:bg-blue-600 active:bg-blue-700 focus:outline-none focus:ring focus:ring-blue-500 rounded-full text-white">
                 Ingresar
             </button>
             <a href="/register/register.php" class="text-sm text-blue-500 hover:underline">Registrarse</a>
@@ -43,7 +38,5 @@
     </form>
 </body>
 
+
 </html>
-
-
-
